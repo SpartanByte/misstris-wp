21e389d334613c99766923c21ed9c71d86ee9296
spartanbyte <briwar10@outlook.com>
2021-05-30 20:38:24 -0500

    added fonts, tested on site header

--- themes/misstris/functions.php
+++ themes/misstris/functions.php
@@ -21,4 +21,29 @@ function my_theme_enqueue_styles() {
         array( $parenthandle ),
         $theme->get( 'Version' ) // this only works if you have Version in the style header
     );
+
+    //Custom Fonts
+    //Lexend Deca (no dependancies = empty array)
+    wp_enqueue_style(
+        'lexend-deca',
+        'https://fonts.googleapis.com/css2?family=Lexend+Deca',
+        array(),
+        '2021'
+    );
+
+    //Henny Penny (no dependancies = empty array)
+        wp_enqueue_style(
+            'henny-penny',
+            'https://fonts.googleapis.com/css2?family=Henny+Penny',
+            array(),
+            '2021'
+        );
+
+    //Font Awesome
+    wp_enqueue_style(
+        'font-awesome',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css',
+        array(),
+        '2021'
+    );
 }
